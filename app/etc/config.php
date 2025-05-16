82d148cef273697e6ba2ef3c521edc60431aef5f
danh126 <danhnt126@gmail.com>
2025-05-16 10:33:46 +0700

    Frontend Test

--- app/etc/config.php
+++ app/etc/config.php
@@ -328,6 +328,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magenest_EventDemo' => 1,
+        'Magenest_FrontendTest' => 1,
         'Magenest_Movie' => 1,
         'Mageplaza_Core' => 1,
         'NTD_CustomerAttribute' => 1,
