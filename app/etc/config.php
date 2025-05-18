ea5925c735bc34a12f5669a378813d091d3d60aa
danh126 <danhnt126@gmail.com>
2025-05-18 21:27:34 +0700

    Custom product section (display section bugs)

--- app/etc/config.php
+++ app/etc/config.php
@@ -329,6 +329,7 @@ return [
         'Magento_WishlistGraphQl' => 1,
         'Magenest_ConfigurableImageChange' => 1,
         'Magenest_CustomAdmin' => 1,
+        'Magenest_CustomProductSection' => 1,
         'Magenest_EventDemo' => 1,
         'Magenest_FrontendTest' => 1,
         'Magenest_Movie' => 1,
