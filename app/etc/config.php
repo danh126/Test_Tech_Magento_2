22832f3a62ddf2b4532c5aa909e1f7bac6d9e3c1
danh126 <danhnt126@gmail.com>
2025-05-18 20:47:48 +0700

    Change logo login backend & admin panel (done change logo login backend)

--- app/etc/config.php
+++ app/etc/config.php
@@ -328,6 +328,7 @@ return [
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
         'Magenest_ConfigurableImageChange' => 1,
+        'Magenest_CustomAdmin' => 1,
         'Magenest_EventDemo' => 1,
         'Magenest_FrontendTest' => 1,
         'Magenest_Movie' => 1,
