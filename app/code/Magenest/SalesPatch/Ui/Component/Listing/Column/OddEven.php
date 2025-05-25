e0b2cfd1b1b9ff3e85e333794bf22c935fd39c15
danh126 <danhnt126@gmail.com>
2025-05-25 11:38:54 +0700

    Fix bugs CSS Odd/Even

--- app/code/Magenest/SalesPatch/Ui/Component/Listing/Column/OddEven.php
+++ app/code/Magenest/SalesPatch/Ui/Component/Listing/Column/OddEven.php
@@ -11,8 +11,12 @@ class OddEven extends Column
     {
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as &$item) {
-                $id = $item['entity_id'] ?? 0;
-                $item[$this->getData('name')] = ($id % 2 === 0) ? 'Even' : 'Odd';
+                $value = ($item['entity_id'] ?? 0) % 2 === 0 ? 'even' : 'odd';
+                if ($value === 'odd') {
+                    $item[$this->getData('name')] = '<span class="grid-severity-critical"><span>' . __('Odd') . '</span></span>';
+                } else {
+                    $item[$this->getData('name')] = '<span class="grid-severity-notice"><span>' . __('Even') . '</span></span>';
+                }
             }
         }
         return $dataSource;
